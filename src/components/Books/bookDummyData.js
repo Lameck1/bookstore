de24c1e24f2eb9899af7434aa55e2a53f96f3ee6
Lameck1 <lameck72@gmail.com>
2021-08-25 22:20:30 +0300

    Add Dummy Books Data

---
+++ src/components/Books/bookDummyData.js
@@ -0,0 +1,21 @@
+export default [{
+  category: 'Action',
+  title: 'The Hunger Games',
+  author: 'Suzanne Collins',
+  progress: 80,
+  currentChapter: 'Chapter 17',
+},
+{
+  category: 'Science Fiction',
+  title: 'Dune',
+  author: 'Frank Herbert',
+  progress: 40,
+  currentChapter: 'Chapter 3: "A Lesson Learned"',
+},
+{
+  category: 'Economy',
+  title: 'Capital in the Twenty-First Century',
+  author: 'Suzanne Collins',
+  progress: 20,
+  currentChapter: 'Introduction',
+}];
